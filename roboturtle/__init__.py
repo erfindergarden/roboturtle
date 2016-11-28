c328878cea3afe19118801d2acb801ecd2b5e300
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-11-28 05:20:31 +0100

    added explicit absolute imports python 2-3 compatibility

--- roboturtle/__init__.py
+++ roboturtle/__init__.py
@@ -1,4 +1,4 @@
-import warnings
+from __future__ import absolute_import
 
 from .client import EchoClient, send_through_socket
 from .server import EchoServer
--- roboturtle/roboserver.py
+++ roboturtle/roboserver.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from roboturtle import EchoServer, RoboTurtle
 import turtle
 import click
